efefe8d9a9c652af7d0cd9b326027ee0afb64932
aether-rootr <aetherrootr@outlook.com>
2021-06-19 16:33:35 +0000

    init project

---
+++ src/main/java/com/bobo_chicke/apparelinventorymanager/ApparelInventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bobo_chicke.apparelinventorymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApparelInventoryManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApparelInventoryManagerApplication.class, args);
+    }
+
+}
