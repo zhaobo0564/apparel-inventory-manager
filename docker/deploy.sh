7e6b93ef5681808fb630ef73439d1124c77d7bb3
aether <1025313354@qq.com>
2021-06-28 11:13:56 +0800

    Update deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,9 +3,9 @@
 set -ex
 VERSION="v0.0.2"
 
-# docker build --no-cache --network=host -t aethertaberu/bobo-chicke-apparel-inventory-manager:${VERSION} .
+docker build --no-cache --network=host -t aethertaberu/bobo-chicke-apparel-inventory-manager:${VERSION} .
 
-# docker tag aethertaberu/bobo-chicke-apparel-inventory-manager:${VERSION} aethertaberu/bobo-chicke-apparel-inventory-manager:latest
+docker tag aethertaberu/bobo-chicke-apparel-inventory-manager:${VERSION} aethertaberu/bobo-chicke-apparel-inventory-manager:latest
 
 docker push aethertaberu/bobo-chicke-apparel-inventory-manager:${VERSION}
 docker push aethertaberu/bobo-chicke-apparel-inventory-manager:latest
